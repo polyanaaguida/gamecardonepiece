ed6e8f3d4a8d182093826efc275a8aa621fa0d11
polyanaaguida <polyanaaguida@gmail.com>
2021-02-06 19:36:07 -0300

    first files

---
+++ atributos.js
@@ -0,0 +1,12 @@
+let cards = [
+    {
+        id: 1,
+        tipo: "A", 
+        nome: "Ruffy",
+        forca: 1,
+        inteligencia: 2,
+        determinacao: 3,
+        akuma: true,
+        img: "assets/imagem.png",
+    },
+]
