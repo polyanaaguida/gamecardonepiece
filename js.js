67447e41398550993ad0f43ae5a52519b843ffc4
polyanaaguida <polyanaaguida@gmail.com>
2021-02-10 21:00:35 -0300

    first card js part1

--- js.js
+++ js.js
@@ -1,1 +1,21 @@
-console.log("Olá Mundo");
+function game() {
+    let app = document.createElement("div");
+    let header = document.createElement("div");
+    let pagetitle = document.createElement("h1");
+    let card = document.createElement("div");
+    let cardtitle = document.createElement("div");
+    let cardicon = document.createElement("img");
+    let cardname = document.createElement("div");
+    let cardtype = document.createElement("div");
+    let cardimg = document.createElement("img");
+    let atributes = document.createElement("div");
+    let detatribute = document.createElement("div");
+    let atkatribute = document.createElement("div");
+    let intatribute = document.createElement("div");
+
+    app.innerHTML = "OI";
+    document.body.appendChild(app);
+    console.log("mundo");
+}
+
+game()
